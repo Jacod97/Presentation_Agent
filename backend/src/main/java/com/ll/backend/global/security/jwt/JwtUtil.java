53b646889f524bd5bc15706a54349dbb1d75f200
son <handsun000@gmail.com>
2025-04-29 00:25:21 +0900

    feat: Jwt 인증

--- backend/src/main/java/com/ll/backend/global/security/jwt/JwtUtil.java
+++ backend/src/main/java/com/ll/backend/global/security/jwt/JwtUtil.java
@@ -1,8 +1,8 @@
 package com.ll.backend.global.security.jwt;
 
 import com.ll.backend.domain.member.member.entity.Member;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -25,26 +25,49 @@ public class JwtUtil {
     @Value("${custom.jwt.refreshExpiration}")
     private Long refreshExpiration;
 
-    public String generateAccessToken(Member member, int type) {
+    public String generateToken(Member member, JwtType type) {
         long now = System.currentTimeMillis();
 
-        if (type == 1) {
-            return Jwts.builder()
-                    .claims(createClaims(member))
-                    .issuedAt(new Date(now))
-                    .expiration(new Date(now + accessExpiration))
-                    .signWith(Keys.hmacShaKeyFor(accessKey.getBytes()))
-                    .compact();
-        }
-        else {
-            return Jwts.builder()
-                    .claims(createClaims(member))
-                    .issuedAt(new Date(now))
-                    .expiration(new Date(now + refreshExpiration))
-                    .signWith(Keys.hmacShaKeyFor(refreshKey.getBytes()))
-                    .compact();
-        }
+        return Jwts.builder()
+                .claims(createClaims(member))
+                .issuedAt(new Date(now))
+                .expiration(new Date(now + type.getExpiration(accessExpiration, refreshExpiration)))
+                .signWith(type.getKey(accessKey, refreshKey))
+                .compact();
+    }
 
+    public boolean validateToken(String token, JwtType type) {
+        try {
+            parseToken(token, type);
+
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public Claims parseToken(String token, JwtType type) {
+        return Jwts.parser()
+                .verifyWith(type.getKey(accessKey, refreshKey))
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public String getUsername(String token, JwtType type) {
+        return Jwts.parser().verifyWith(type.getKey(accessKey, refreshKey)).build().parseSignedClaims(token).getPayload().get("username", String.class);
+    }
+
+    public String getNickname(String token, JwtType type) {
+        return Jwts.parser().verifyWith(type.getKey(accessKey, refreshKey)).build().parseSignedClaims(token).getPayload().get("nickname", String.class);
+    }
+
+    public String getRole(String token, JwtType type) {
+        return Jwts.parser().verifyWith(type.getKey(accessKey, refreshKey)).build().parseSignedClaims(token).getPayload().get("role", String.class);
+    }
+
+    public Long getMemberId(String token, JwtType type) {
+        return Jwts.parser().verifyWith(type.getKey(accessKey, refreshKey)).build().parseSignedClaims(token).getPayload().get("memberId", Long.class);
     }
 
     private Map<String, ?> createClaims(Member member) {
