b577dcbdb8d9c281fd09970443bceb45cb06305b
handsun000 <handsun000>
2025-10-27 16:28:18 +0900

    add lombok

--- backend/src/main/java/com/ll/backend/domain/member/member/entity/Member.java
+++ backend/src/main/java/com/ll/backend/domain/member/member/entity/Member.java
@@ -1,14 +1,17 @@
 package com.ll.backend.domain.member.member.entity;
 
+import com.ll.backend.global.entity.BaseTime;
 import io.jsonwebtoken.Claims;
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
-public class Member {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member extends BaseTime {
 
     @Column(nullable = false,unique=true)
     String username;
@@ -17,68 +20,10 @@ public class Member {
     String email;
     String role;
 
-    public Member() {}
-
-    public Member(String username, String password, String nickname, String email, String role) {
-        this.username = username;
-        this.password = password;
-        this.nickname = nickname;
-        this.email = email;
-        this.role = role;
-    }
-
     public Member(Claims claims) {
-        this.id = claims.get("memberId", Long.class);
+        setId(claims.get("memberId", Long.class));
         this.username = claims.get("username", String.class);
         this.role = claims.get("role", String.class);
         this.nickname = claims.get("nickname", String.class);
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getNickname() {
-        return nickname;
-    }
-
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
     }
 }
